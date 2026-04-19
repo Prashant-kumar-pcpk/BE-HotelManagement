720a5e265f9f695632874813ae5d5ce4ae294514
Prashant-kumar-pcpk <prashantkumar.pcpk@gmail.com>
2026-04-20 03:23:49 +0530

    added changes

--- controller/userController.js
+++ controller/userController.js
@@ -76,6 +76,22 @@ exports.login = async (req, res) =>{
     }
 }
 
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({}, "-password").sort({ createdAt: -1 });
+
+        res.status(200).json({
+            message: "Users fetched successfully",
+            count: users.length,
+            users
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
 exports.getHome = async (req, res) =>{
     
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,13 +3,14 @@ const express = require("express");
 const router = express.Router(); // enable the routing
 
 // import the controller
-const {registerUser, login, getHome} = require("../controller/userController");
+const {registerUser, login, getHome, getAllUsers} = require("../controller/userController");
 const {createContact} = require("../controller/contactController")
 
 
 //  routes
 router.post("/register", registerUser);
 router.post("/login", login);
+router.get("/all", getAllUsers);
 router.get("/home", getHome);
 
 // contact
